989928c10463ffd509f282c2f96b899daa8553cf
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2026-06-27 10:07:26 -0500

    Initial Commit

---
+++ ForbiddenIslandIpad/ForbiddenIslandIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForbiddenIslandIpadApp.swift
+//  ForbiddenIslandIpad
+//
+//  Created by Scott Miller on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ForbiddenIslandIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
